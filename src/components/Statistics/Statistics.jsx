5d335db44cdebf2c3c4c86b1df3ad9ea6242eeaa
Kateryna1706 <silckatya@gmail.com>
2023-06-22 17:52:11 +0200

    did two tasks

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,19 @@
+import css from './Statistics.module.css';
+import { getRandomHexColor } from 'utils/getRandomHexColor.js';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      <h2 className={css.title}>{title}</h2>
+
+      <ul className={css.statList}>
+        {stats.map(stat => (
+          <li className={css.item} key={stat.id} style={{backgroundColor: getRandomHexColor()}}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
