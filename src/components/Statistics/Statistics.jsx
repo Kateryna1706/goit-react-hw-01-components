e0ae0aef50818d65df3c46e9f1faeacf1e836a97
Kateryna1706 <silckatya@gmail.com>
2023-06-23 15:09:06 +0200

    fixed

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,8 +5,7 @@ import PropTypes from 'prop-types';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
-
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         {stats.map(stat => (
           <li
